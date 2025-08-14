8a4127561809f6ecd039f8b3700736dba7924659
RatoMolhado73 <bruno.73957@aluno.pr.senac.br>
2025-08-14 15:34:36 -0300

    mudando rotação e imagem

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './conguitos.webp';
 import './App.css';
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Ola Mundo!
         </a>
       </header>
     </div>
